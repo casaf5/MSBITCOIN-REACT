8b074a0e281dd7d59911d1f8eb488a886946d8d6
Asaf Cohen <casaf5@gmail.com>
2020-08-23 15:27:52 +0300

    Updates

---
+++ precache-manifest.e357beae9bb118325193f7934a1f40f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7baafc88f5ad7ab68f7759d1f5d3a678",
+    "url": "/MSBITCOIN-REACT/index.html"
+  },
+  {
+    "revision": "be77cc80d0f7f6e325cd",
+    "url": "/MSBITCOIN-REACT/static/css/main.b726a54f.chunk.css"
+  },
+  {
+    "revision": "fe059ee4c6fa10ccff23",
+    "url": "/MSBITCOIN-REACT/static/js/2.df7e9da0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MSBITCOIN-REACT/static/js/2.df7e9da0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be77cc80d0f7f6e325cd",
+    "url": "/MSBITCOIN-REACT/static/js/main.c0ed1434.chunk.js"
+  },
+  {
+    "revision": "a9bd872ca0cedf3881ee",
+    "url": "/MSBITCOIN-REACT/static/js/runtime-main.1b48f996.js"
+  },
+  {
+    "revision": "a1b815da4de43411b06e13acb2031547",
+    "url": "/MSBITCOIN-REACT/static/media/bg.a1b815da.svg"
+  }
+]);
